6293458af40f78ea51f0cf22f74e5b4cc1edebfd
zqtao <453395742@qq.com>
2018-12-07 22:16:22 +0800

    1、跨域问题     使用代理进行解决 axios     post请求不要使用axios.post()这个方法别名

--- config/index.js
+++ config/index.js
@@ -10,7 +10,19 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': ''
+        }
+      },
+      '/ws/*': {
+        target: 'ws://127.0.0.1:8082',
+        ws: true
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+    <button @click="test_get">get</button>
+    <button @click="test_post">get</button>
+
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    test_get: function () {
+      axios.get('/league/selectleaguebyid?sid=1606915055').then(resp => {
+        console.log(resp)
+      })
+    },
+    test_post: function () {
+      axios({
+        method: 'post',
+        url: '/league/test',
+        data: {
+          "leagueId": "63ef4747e5744a5e990f92a76f586790",
+          "college": "计算机与信息工程",
+          "major": "计算机科学与技术",
+          "className": "计科2班",
+          "studentId": "1606915055",
+          "lname": "恩超",
+          "sex": "女",
+          "date": "1997/10/12",
+          "idnumber": "411524133202033612",
+          "nation": "汉族·",
+          "nativePlace": "河南南阳宛城区",
+          "politicalStatus": "团员",
+          "years": "2018/01/02",
+          "place": "南阳理工学院",
+          "letter": "有",
+          "zheng": "有",
+          "memberProfile": "有",
+          "phone": "15139081588",
+          "qq": "453395685",
+          "remark": "班长",
+          "register": "是",
+          "createTime": "2018/09/09",
+          "lastEditTime": "2018/10/10"
+        }
+      }).then(resp => {
+        console.log(resp)
+      })
+    }
+  }
 }
 </script>
 
