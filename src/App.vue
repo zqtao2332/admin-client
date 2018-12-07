286ac19490c87a55cc47bdf4c4fee77c94562600
zqtao <453395742@qq.com>
2018-12-08 03:26:15 +0800

    引入 SUI Mobile

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,41 @@
 <template>
-  <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <!--<router-view/>-->
-    <button @click="test_get">get</button>
-    <button @click="test_post">get</button>
-
+  <div class="page-group">
+    <div class="page page-current">
+      <!-- 工具栏 -->
+      <nav class="bar bar-tab">
+        <a class="tab-item external active" href="#">
+          <span class="icon icon-home"></span>
+          <span class="tab-label"><router-link to="/">首页</router-link></span>
+        </a>
+        <a class="tab-item external" href="#">
+          <span class="icon icon-star"></span>
+          <span class="tab-label"><router-link to="/new_tuany">新人</router-link></span>
+        </a>
+        <a class="tab-item external" href="#">
+          <span class="icon icon-friends"></span>
+          <span class="tab-label"><router-link to="/develop_turany">发展</router-link></span>
+        </a>
+        <a class="tab-item external" href="#">
+          <span class="icon icon-emoji"></span>
+          <span class="tab-label"><router-link to="/excellent_tuany">推优</router-link></span>
+        </a>
+        <a class="tab-item external" href="#">
+          <span class="icon icon-me"></span>
+          <span class="tab-label"><router-link to="/me">个人中心</router-link></span>
+        </a>
+      </nav>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
-export default {
-  name: 'App',
-  methods: {
-    test_get: function () {
-      axios.get('/league/selectleaguebyid?sid=1606915055').then(resp => {
-        console.log(resp)
-      })
-    },
-    test_post: function () {
-      axios({
-        method: 'post',
-        url: '/league/test',
-        data: {
-          "leagueId": "63ef4747e5744a5e990f92a76f586790",
-          "college": "计算机与信息工程",
-          "major": "计算机科学与技术",
-          "className": "计科2班",
-          "studentId": "1606915055",
-          "lname": "恩超",
-          "sex": "女",
-          "date": "1997/10/12",
-          "idnumber": "411524133202033612",
-          "nation": "汉族·",
-          "nativePlace": "河南南阳宛城区",
-          "politicalStatus": "团员",
-          "years": "2018/01/02",
-          "place": "南阳理工学院",
-          "letter": "有",
-          "zheng": "有",
-          "memberProfile": "有",
-          "phone": "15139081588",
-          "qq": "453395685",
-          "remark": "班长",
-          "register": "是",
-          "createTime": "2018/09/09",
-          "lastEditTime": "2018/10/10"
-        }
-      }).then(resp => {
-        console.log(resp)
-      })
+  export default {
+    name: 'App',
+    components: {
     }
   }
-}
+
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Admin from "../components/Admin";
+import NewTuany from "../components/NewTuany";
+import DevelopTurany from "../components/DevelopTurany";
+import ExcellentTuany from "../components/ExcellentTuany";
+import Me from "../components/Me";
 
 Vue.use(Router)
 
@@ -8,8 +12,29 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Admin',
+      component: Admin
+    },
+    {
+      path: '/new_tuany',
+      name: 'NewTuany',
+      component: NewTuany
+    },
+    {
+      path: '/develop_turany',
+      name: 'DevelopTurany',
+      component: DevelopTurany
+    },
+    {
+      path: '/excellent_tuany',
+      name: 'ExcellentTuany',
+      component: ExcellentTuany
+    },
+    {
+      path: '/me',
+      name: 'Me',
+      component: Me
     }
+
   ]
 })
